1088ef6e77a57ba8a3203eb36a430787fbf790cd
CrushCola <ranchangxin@gmail.com>
2025-03-03 16:42:59 +0800

    添加请求日志记录

--- go-fiber-server/internal/middleware/timeout/timeout.go
+++ go-fiber-server/internal/middleware/timeout/timeout.go
@@ -2,6 +2,7 @@ package timeout
 
 import (
 	"context"
+	"fmt"
 	"github.com/gofiber/fiber/v3"
 	"go-fiber-ent-web-layout/internal/tools"
 	"go-fiber-ent-web-layout/pkg/pool"
@@ -13,27 +14,28 @@ import (
 // 向请求ctx中set一个WithTimeout的Context
 func NewMiddleware(timeout time.Duration) fiber.Handler {
 	return func(c fiber.Ctx) error {
+		beforeTime := time.Now().UnixMilli()
 		ctx, cancel := context.WithTimeout(c.UserContext(), timeout)
-		defer cancel()
-
+		defer func() {
+			cancel()
+			afterTime := time.Now().UnixMilli()
+			slog.Info(fmt.Sprintf("[method:%s uri:%s] 处理耗时：%dms", c.Method(), c.OriginalURL(), afterTime-beforeTime))
+		}()
 		c.SetUserContext(ctx)
-
 		ch := make(chan error)
-		defer close(ch)
-
 		pool.DoGo(context.Background(), func(ctx context.Context, err any) {
-			slog.Error("协程池请求处理异常", "error", err, "uri", c.OriginalURL(), "method", c.Method())
+			slog.Error("协程池请求处理异常", "error", err)
 			ch <- tools.FiberServerError("请求处理失败")
 		}, func() {
 			ch <- c.Next()
 		})
-
 		select {
 		// 如果请求正常完成那么直接返回
 		case err := <-ch:
 			return err
 		// 返回请求超时错误
 		case <-ctx.Done():
+			slog.ErrorContext(ctx, "请求处理超时", "uri", c.OriginalURL(), "method", c.Method())
 			return fiber.ErrRequestTimeout
 		}
 	}
